ef0fdf6bc4e3b071856bc004d689c991def4e503
fois2003 <fois2010@gmail.com>
2025-12-26 13:11:45 -0300

    Changed frontend port to 5174

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
         emptyOutDir: true,
     },
     server: {
-        port: 5173,
+        port: 5174,
         proxy: {
             '/api': {
-                target: 'http://localhost:3000',
+                target: 'http://localhost:3002',
                 changeOrigin: true,
             },
         },
